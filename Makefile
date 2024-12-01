ab469add588e4670401251c6e73788c4feb026ae
pablodamascenoo <pablofacex@gmail.com>
2024-11-30 21:29:42 -0300

    feat: added menu to application

--- Makefile
+++ Makefile
@@ -1,5 +1,30 @@
-all: funcs.o
-	gcc -o funcs funcs.o main.c -o main
+# Nome do executável
+TARGET = main
 
-funcs.o: funcs.h definitions.h
-	gcc -c funcs.c
+# Compilador
+CC = gcc
+
+# Opções de compilação
+CFLAGS = -Wall -std=c11
+
+# Arquivos de objeto
+OBJS = main.o funcs.o
+
+# Regra padrão para compilar o projeto
+all: $(TARGET)
+
+# Regra para gerar o executável
+$(TARGET): $(OBJS)
+	$(CC) $(OBJS) -o $(TARGET) -lm
+
+# Regra para compilar o main.c
+main.o: main.c funcs.h definitions.h
+	$(CC) $(CFLAGS) -c main.c
+
+# Regra para compilar o funcs.c
+funcs.o: funcs.c funcs.h definitions.h
+	$(CC) $(CFLAGS) -c funcs.c
+
+# Regra para limpar os arquivos gerados
+clean:
+	rm -f $(OBJS) $(TARGET)
--- main.c
+++ main.c
@@ -1,18 +1,74 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 #include "definitions.h"
 #include "funcs.h"
 
-int main (int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
+    // Configurar o locale para UTF-8
+    setlocale(LC_ALL, "pt_BR.UTF-8");
 
-    No* raiz = inicializar_arvore();
+    No *raiz = inicializar_arvore();
 
     raiz = carregar_livros("livros.csv", raiz);
-    
-    exibir_arvore(raiz);
+
+    int command = 0;
+
+    do
+    {
+        printf("\n\n+========================+\n");
+        printf("|| 1 - Inserir livro    ||\n");
+        printf("|| 2 - Buscar por autor ||\n");
+        printf("|| 3 - Exibir arvore    ||\n");
+        printf("|| 4 - Sair             ||\n");
+        printf("+========================+\n\n");
+        printf("Digite o comando: ");
+
+        scanf("%d", &command);
+
+        switch (command)
+        {
+        case 1:
+        {
+            livro livro;
+            printf("Digite o codigo do livro: ");
+            scanf("%d", &livro.codigo);
+            printf("Digite o titulo do livro: ");
+            scanf(" %[^\n]", livro.titulo);
+            printf("Digite o autor do livro: ");
+            scanf(" %[^\n]", livro.autor);
+            printf("Digite o genero do livro: ");
+            scanf(" %[^\n]", livro.genero);
+            printf("Digite o ano do livro: ");
+            scanf("%d", &livro.ano);
+            printf("Digite a editora do livro: ");
+            scanf(" %[^\n]", livro.editora);
+            printf("Digite o numero de paginas do livro: ");
+            scanf("%d", &livro.paginas);
+            printf("Livro inserido com sucesso!\n");
+            inserir_livro(&raiz, livro);
+        }
+        break;
+        case 2:
+        {
+            char autor[40];
+            printf("Digite o autor que deseja buscar: ");
+            scanf(" %[^\n]", autor);
+            buscar_por_autor(raiz, autor);
+        }
+        break;
+        case 3:
+            exibir_arvore(raiz);
+            break;
+        case 4:
+            liberar_arvore(raiz);
+            break;
+        default:
+            printf("Comando invalido\n");
+        }
+    } while (command != 4);
 
     return 0;
 }
